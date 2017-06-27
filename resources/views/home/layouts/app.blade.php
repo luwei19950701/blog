4a2f58c1f4ac0db131d9d9f74a213543e1db302c
smile <1197329636@qq.com>
2017-06-27 15:49:37 +0800

    laravel blog init

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -6,45 +6,24 @@
     <meta http-equiv="Content-Language" content="zh-CN">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <title>不落阁 - 一个.NET程序员的个人博客网站</title>
-    <link rel="shortcut icon" href="../images/Logo_40.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../home/images/Logo_40.png" type="image/x-icon">
     <!--Layui-->
-    <link href="../plug/layui/css/layui.css" rel="stylesheet" />
+    <link href="../home/plug/layui/css/layui.css" rel="stylesheet" />
     <!--font-awesome-->
-    <link href="../plug/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../home/plug/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
     <!--全局样式表-->
-    <link href="../css/global.css" rel="stylesheet" />
+    <link href="../home/css/global.css" rel="stylesheet" />
     <!-- 本页样式表 -->
-    <link href="../css/home.css" rel="stylesheet" />
+    <link href="../home/css/home.css" rel="stylesheet" />
 </head>
 <body>
-    @@include()'home.layouts.nav')
+    @include('home.layouts.header')
 
     @yield('content')
 
+    @include('home.layouts.footer')
 
-    <!-- 底部 -->
-    <footer class="blog-footer">
-        <p><span>Copyright</span><span>&copy;</span><span>2017</span><a href="http://www.lyblogs.cn">不落阁</a><span>Design By LY</span></p>
-        <p><a href="http://www.miibeian.gov.cn/" target="_blank">蜀ICP备16029915号-1</a></p>
-    </footer>
-    <!--侧边导航-->
-    <ul class="layui-nav layui-nav-tree layui-nav-side blog-nav-left layui-hide" lay-filter="nav">
-        <li class="layui-nav-item layui-this">
-            <a href="home.html"><i class="fa fa-home fa-fw"></i>&nbsp;网站首页</a>
-        </li>
-        <li class="layui-nav-item">
-            <a href="article.html"><i class="fa fa-file-text fa-fw"></i>&nbsp;文章专栏</a>
-        </li>
-        <li class="layui-nav-item">
-            <a href="resource.html"><i class="fa fa-tags fa-fw"></i>&nbsp;资源分享</a>
-        </li>
-        <li class="layui-nav-item">
-            <a href="timeline.html"><i class="fa fa-road fa-fw"></i>&nbsp;点点滴滴</a>
-        </li>
-        <li class="layui-nav-item">
-            <a href="about.html"><i class="fa fa-info fa-fw"></i>&nbsp;关于本站</a>
-        </li>
-    </ul>
+    @include('home.layouts.nav')
     <!--分享窗体-->
     <div class="blog-share layui-hide">
         <div class="blog-share-body">
@@ -61,10 +40,10 @@
     <!--遮罩-->
     <div class="blog-mask animated layui-hide"></div>
     <!-- layui.js -->
-    <script src="../plug/layui/layui.js"></script>
+    <script src="../home/plug/layui/layui.js"></script>
     <!-- 全局脚本 -->
-    <script src="../js/global.js"></script>
+    <script src="../home/js/global.js"></script>
     <!-- 本页脚本 -->
-    <script src="../js/home.js"></script>
+    <script src="../home/js/home.js"></script>
 </body>
 </html>
